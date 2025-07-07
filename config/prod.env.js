ec06eceb81b6f8093e690d4c725089c5f82d0933
郑浩龙 <zhenghaolong@zhenghaolongdeMac-mini.local>
2025-07-07 21:51:28 +0800

    mrpc链id

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,17 +6,17 @@ module.exports = {
   VUE_APP_API_BASE_URL: '"https://test-vote.hashahead.org/dev-api/"',
 
   // 生产环境链配置
-  VUE_APP_MAINNET_CHAIN_ID: '"0x2BF"', // 703 (修复：使用正确的大写格式)
-  VUE_APP_MAINNET_RPC_URL: '"https://rpc.hashahead.org"', // 修复：主链应该使用主RPC
+  VUE_APP_MAINNET_CHAIN_ID: '"0x329"', // 809 (修复：使用正确的链ID)
+  VUE_APP_MAINNET_RPC_URL: '"https://rpc.hashahead.org/mrpc"', // 修复：主链使用/mrpc
   VUE_APP_MAINNET_CHAIN_NAME: '"Hash Ahead Mainnet"',
   VUE_APP_MAINNET_EXPLORER_URL: '"https://scan.hashahead.org/"',
 
-  VUE_APP_SUBCHAIN_CHAIN_ID: '"0x2C0"', // 704 (修复：使用正确的大写格式)
-  VUE_APP_SUBCHAIN_RPC_URL: '"https://rpc.hashahead.org/mrpc"', // 修复：应用链使用mrpc
+  VUE_APP_SUBCHAIN_CHAIN_ID: '"0x32A"', // 810 (修复：子链ID应该是主链ID+1)
+  VUE_APP_SUBCHAIN_RPC_URL: '"https://rpc.hashahead.org"', // 修复：应用链不使用mrpc
   VUE_APP_SUBCHAIN_CHAIN_NAME: '"Hash Ahead ByteBloom"',
   VUE_APP_SUBCHAIN_EXPLORER_URL: '"https://scan.hashahead.org/"',
 
   // 兼容旧版本的变量名
-  VUE_APP_APPCHAIN_CHAIN_ID: '"0x2C0"',
-  VUE_APP_APPCHAIN_RPC_URL: '"https://rpc.hashahead.org/mrpc"'
+  VUE_APP_APPCHAIN_CHAIN_ID: '"0x32A"',
+  VUE_APP_APPCHAIN_RPC_URL: '"https://rpc.hashahead.org"'
 }
